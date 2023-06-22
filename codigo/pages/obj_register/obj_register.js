895ca3db68106921f52671cd2261d68b16dfe21e
smkpuc <1280955@sga.pucminas.br>
2023-06-22 14:46:59 -0300

    fix obj register

--- codigo/pages/obj_register/obj_register.js
+++ codigo/pages/obj_register/obj_register.js
@@ -1,20 +1,34 @@
 let button = document.getElementById('button_register');
 
+let list_obj = [];
+
 button.addEventListener("click", function(e) {
     e.preventDefault()
+
+    let list_obj = [];
 
     let name = document.getElementById('objname').value;
     let local = document.getElementById('objlocal').value;
     let date = document.getElementById('objdate').value;
     let description = document.getElementById('objdesc').value;
 
-    let listobj = {
+    let old_objects = JSON.parse(localStorage.getItem("@tib-objects"));
+
+    if(old_objects) {
+        old_objects.forEach((object) => {
+            list_obj.push(object)
+        });
+    }
+
+    let obj = {
         nome: name,
         local: local,
         data: date,
-        descricao: description
+        descricao: description,
     }
     
-    localStorage.setItem("@tib-objects", JSON.stringify(listobj));
+    list_obj.push(obj)
+    
+    localStorage.setItem("@tib-objects", JSON.stringify(list_obj));
     alert("Objeto cadastrado com sucesso!")
 })
