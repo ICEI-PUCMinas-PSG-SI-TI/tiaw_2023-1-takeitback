92fb0b77b5573af39de7256b5fc56d2bf2f926b4
Wesley <wesley_henrique15@hotmail.com>
2023-06-26 01:52:43 -0300

    UPDATE obj_register

--- codigo/pages/obj_register/obj_register.js
+++ codigo/pages/obj_register/obj_register.js
@@ -1,37 +1,91 @@
-let button = document.getElementById('button_register');
-
-button.addEventListener("click", function(e) {
-    e.preventDefault()
-
+function saveObj() {
     let list_obj = [];
-
-    let name = document.getElementById('objname').value;
-    let local = document.getElementById('objlocal').value;
-    let date = document.getElementById('objdate').value;
-    let description = document.getElementById('objdesc').value;
-    let type = 1;
-
-
     let old_objects = JSON.parse(localStorage.getItem("@tib-objects"));
 
-    if(old_objects) {
+    if (old_objects) {
         old_objects.forEach((object) => {
             list_obj.push(object)
         });
     }
 
     let obj = {
-        nome: name,
-        local: local,
-        data: date,
-        descricao: description,
+        nome: document.getElementById('objname').value,
+        local: document.getElementById('objlocal').value,
+        data: document.getElementById('objdate').value,
+        descricao: document.getElementById('objdesc').value,
+        categoria: parseInt(document.getElementById('objcategory').value),
         id: list_obj.length + 1,
         tipo: type,
+        usuario: JSON.parse(localStorage.getItem("userLogged"))
     }
-    
-    list_obj.push(obj)
-    
+
+    list_obj.push(obj);
+
     localStorage.setItem("@tib-objects", JSON.stringify(list_obj));
     alert("Objeto cadastrado com sucesso!")
-    location.href = "../obj_register/obj_register.html"
-})
+    form.reset();
+}
+function changeType(value, btn) {
+    let buttons = document.getElementsByClassName("page-link");
+    for (let i = 0; i < buttons.length; i++) {
+        if (buttons[i].classList.contains("active")) buttons[i].classList.remove("active");
+    }
+    btn.classList.add("active");
+
+    let word = value === 1 ? "perdido" : "achado";
+    document.getElementById("title").innerHTML = "Objeto " + word;
+    document.getElementById("labelLocal").innerHTML = "Local onde foi " + word;
+    document.getElementById("labelDate").innerHTML = "Data em que foi " + word;
+    type = value;
+}
+
+function checkValidation(element) {
+    if (element.id != "file") {
+        element.className += " is-invalid";
+    }
+    function classValid() {
+        element.classList.remove("is-invalid");
+    }
+    if (element.id == "objname" && element.value.length >= 3) {
+        classValid();
+        return true;
+    }
+    if (element.id == "objlocal" && element.value.length >= 3) {
+        classValid();
+        return true;
+    }
+    if (element.id == "objdate") {
+        let date = new Date(element.value);
+        if (!isNaN(date.getTime())) {
+            classValid();
+            return true;
+        }
+    }
+    if (element.id == "objdesc" && element.value.length >= 5) {
+        classValid();
+        return true;
+    }
+}
+let type = 1;
+
+const inputs = document.querySelectorAll("input");
+inputs.forEach(element => {
+    element.addEventListener("input", e => {
+        element.classList.remove("is-invalid");
+        checkValidation(element);
+    });
+});
+let form = document.getElementById("form_obj");
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    event.stopPropagation();
+    let contValid = 0;
+    inputs.forEach(element => {
+        if (checkValidation(element)) {
+            contValid++;
+        }
+    })
+    if (contValid == 4) {
+        saveObj();
+    }
+});
